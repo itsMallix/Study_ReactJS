a1f824f5b015b34e20d79101e847580587b8b972
itsMallix <amandamaulana00@gmail.com>
2025-06-30 10:38:56 +0700

    add folder belajar

---
+++ belajar-react-dasar/src/hello-world/HelloWorld.jsx
@@ -0,0 +1,12 @@
+function HelloWorld(){
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Day 1 balajar react loh ya 🤭</p>
+        </div>
+    )
+}
+
+export default HelloWorld
+
+//return function hanya bisa return 1 nilai saja jika ingin return 2 nilai bis apakai div / fragment <>
